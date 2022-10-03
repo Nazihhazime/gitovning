ffb519a6da50d5d2a251f875198bf191d1d67af7
januex <jankenmatene933@gmail.com>
2022-10-03 20:41:05 +0200

    small fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>hej hej</p>
+        <p>hej hej hej</p>
         <a
           className="App-link"
           href="https://reactjs.org"
